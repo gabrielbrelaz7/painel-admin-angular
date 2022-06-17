5f70d0ec12f508287bc00b4634bfdf380c5a66e1
Gabriel Brelaz <gabrielbrelaz@gmail.com>
2022-06-17 17:42:42 -0300

    Aplicando autenticação

--- painel/src/app/app-routing.module.ts
+++ painel/src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './layouts/auth/auth.component';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { HomepageComponent } from './modules/homepage/homepage.component';
+import { LoginComponent } from './modules/login/login.component';
+import { AuthGuard } from './shared/auth/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+		path:'',
+		component: DashboardComponent,
+		children: [
+      {
+				path:'',
+				component: HomepageComponent,
+			},
+		],
+    canActivate:[AuthGuard]
+    
+	},
+
+	{
+		path:'',
+		component: AuthComponent,
+		children: [
+			{path: '', redirectTo: 'login', pathMatch: 'full'},
+			{path: 'login', component: LoginComponent},
+		]
+	},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- painel/src/app/app.module.ts
+++ painel/src/app/app.module.ts
@@ -1,20 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NbThemeModule } from '@nebular/theme';
+import { RouterModule } from '@angular/router';
+import { NbButtonModule, NbLayoutModule, NbSidebarModule, NbSidebarService, NbThemeModule } from '@nebular/theme';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { AuthComponent } from './layouts/auth/auth.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { LoginComponent } from './modules/login/login.component';
+import { HomepageComponent } from './modules/homepage/homepage.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AuthComponent,
+    HeaderComponent,
+    SidebarComponent,
+    LoginComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+
+    RouterModule,
+    NbLayoutModule,
+    NbSidebarModule,
+    NbButtonModule,
+    NbEvaIconsModule,
+    
     NbThemeModule.forRoot()
   ],
-  providers: [],
+  providers: [NbSidebarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
