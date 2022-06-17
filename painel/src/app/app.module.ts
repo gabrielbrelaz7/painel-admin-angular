1386eed631b5f901e60606a6e1e3592ac56eeef0
Gabriel Brelaz <gabrielbrelaz@gmail.com>
2022-06-17 14:54:02 -0300

    Getting Started

---
+++ painel/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NbThemeModule } from '@nebular/theme';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NbThemeModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
